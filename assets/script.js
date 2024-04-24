381a50c47e23636414dc56fbe6c28d022a17d274
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-24 15:25:55 -0600

    styled main website(made like 6 months ago resued)

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//no js? No way, maybe I should write an malware auto installer.
+let starContainer = document.querySelector(".stars");
+
+for (let i = 0; i < 100; i++) {
+  starContainer.innerHTML += `<div class="star"></div>`;
+}
+// update! theres javascipt now! So i can obfusicate everything and then install the malware!
