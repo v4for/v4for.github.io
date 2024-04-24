bfe4b268a6817d071e95f5e8dc1e0b667b1d25bb
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-24 16:13:49 -0600

    404 rewrite

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,1 @@
 //no js? No way, maybe I should write an malware auto installer.
-let starContainer = document.querySelector(".stars");
-
-for (let i = 0; i < 100; i++) {
-  starContainer.innerHTML += `<div class="star"></div>`;
-}
-// update! theres javascipt now! So i can obfusicate everything and then install the malware!
