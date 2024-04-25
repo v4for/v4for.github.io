ee658a93da0bc2d38e309e55dccf8e1526367484
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-24 23:33:56 -0600

    no

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,22 @@
-//lets do this one more time shall we? my name is
-const observerOptions = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.7
-  };
-  
-  function observerCallback(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        // fade in observed elements that are in view
-        entry.target.classList.replace('fadeOut', 'fadeIn');
-      } else {
-        // fade out observed elements that are not in view
-        entry.target.classList.replace('fadeIn', 'fadeOut');
-      }
-    });
-  }
-  
-  const observer = new IntersectionObserver(observerCallback, observerOptions);
-  
-  const fadeElms = document.querySelectorAll('.adbox');
-  fadeElms.forEach(el => observer.observe(el));
+//lets do this one more time shall we? my name is i had to rewrite everything this better work
+
+
+const fadeIns = document.querySelectorAll('.fade-in');
+
+const fadeInOptions = {
+  threshold: 0.5 
+};
+
+const fadeInObserver = new IntersectionObserver((entries, fadeInObserver) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+    } else {
+      entry.target.style.opacity = 0;
+    }
+  });
+}, fadeInOptions);
+
+fadeIns.forEach(fadeIn => {
+  fadeInObserver.observe(fadeIn);
+});
