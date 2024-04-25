e00434b8f93fd20c2a2402cb646e7805df737ab5
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-24 21:41:56 -0600

    minecraft ahh splash page

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,16 @@
-    var phrases = [
-      'Do your IT "work" here. Dev work in progress',
-      "To actaully learn IT go to w3",
-      "Minecraft ahh vibes",
-      "The answer is 42",
-      "Linux on top (that's right Martin)",
-      'Wow such difficult "education',
-      "Bro dont clown my dev build"
-    ];
+var phrases = [
+    'Do your IT "work" here. Dev work in progress',
+    "To actually learn IT, go to W3",
+    "Minecraft ahh vibes",
+    "The answer is 42",
+    "Linux on top (that's right Martin)",
+    'Wow, such difficult "education"',
+    "Bro, don't clown my dev build"
+  ];
   
-    var randomIndex = Math.floor(Math.random() * phrases.length);
-    var randomPhrase = phrases[randomIndex];
+  var randomIndex = Math.floor(Math.random() * phrases.length);
+  var randomPhrase = phrases[randomIndex];
   
-    var bannerSubtitle = document.querySelector('.banner-subtitle');
-    if (bannerSubtitle) {
-      bannerSubtitle.textContent = randomPhrase;
-    }
+  var bannerSubtitle = document.getElementsByClassName("banner-subtitle")[0];
+  bannerSubtitle.innerHTML = randomPhrase;
+  
