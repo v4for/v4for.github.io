e6033201df6ab901d6658b0255d8ce37dcc146da
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-24 21:28:22 -0600

    splash tags like mineceraft

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
-//no js? No way, maybe I should write an malware auto installer.
+    var phrases = [
+      'Do your IT "work" here. Dev work in progress',
+      "To actaully learn IT go to w3",
+      "Minecraft ahh vibes",
+      "The answer is 42",
+      "Linux on top (that's right Martin)",
+      'Wow such difficult "education',
+      "Bro dont clown my dev build"
+    ];
+  
+    var randomIndex = Math.floor(Math.random() * phrases.length);
+    var randomPhrase = phrases[randomIndex];
+  
+    var bannerSubtitle = document.querySelector('.banner-subtitle');
+    if (bannerSubtitle) {
+      bannerSubtitle.textContent = randomPhrase;
+    }
