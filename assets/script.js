bbcecfb30787317f9b75f3dcaac8212376dea3ad
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-24 23:21:59 -0600

    no im to tired i just need to get ths

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,23 @@
 //lets do this one more time shall we? my name is
+const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.7
+  };
+  
+  function observerCallback(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // fade in observed elements that are in view
+        entry.target.classList.replace('fadeOut', 'fadeIn');
+      } else {
+        // fade out observed elements that are not in view
+        entry.target.classList.replace('fadeIn', 'fadeOut');
+      }
+    });
+  }
+  
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  
+  const fadeElms = document.querySelectorAll('.adbox');
+  fadeElms.forEach(el => observer.observe(el));
