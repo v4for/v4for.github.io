c08ec33cf51418ee65ee2c853845fcafcbdb235c
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-24 22:10:14 -0600

    fnally work?

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,1 @@
-var phrases = [
-    'Do your IT "work" here. Dev work in progress',
-    "To actually learn IT, go to W3",
-    "Minecraft ahh vibes",
-    "The answer is 42",
-    "Linux on top (that's right Martin)",
-    'Wow, such difficult "education"',
-    "Bro, don't clown my dev build"
-  ];
-  
-  var randomIndex = Math.floor(Math.random() * phrases.length);
-  var randomPhrase = phrases[randomIndex];
-  
-  var bannerSubtitle = document.getElementById("changetext");
-  bannerSubtitle.innerHTML = randomPhrase;
-  
+//yeah back to no js where its supposed to be
