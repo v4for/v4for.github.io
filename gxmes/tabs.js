8dbdcb5ad136ebd9ee917f779dc21e509c3e8100
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-30 16:23:34 -0600

    Update tabs.js

--- gxmes/tabs.js
+++ gxmes/tabs.js
@@ -111,3 +111,21 @@ function disablePopupBehavior() {
         link.onclick = null;
     });
 }
+
+
+// freaking tab cloak being retarted 
+
+window.onload = function() {
+  var savedTabName = localStorage.getItem('tabName');
+  var savedTabImage = localStorage.getItem('tabImage');
+  if (savedTabName) {
+    document.title = savedTabName;
+  }
+  if (savedTabImage) {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = savedTabImage;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+}
