0e12f1aca7383d05424149520b1a0a39fa4deb37
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-30 16:59:02 -0600

    added about blank ono load

--- s/settings.js
+++ s/settings.js
@@ -1,1 +1,36 @@
+var blank = localStorage.getItem('blank') === 'true';
+var button = document.getElementById("toggleButton");
 
+function toggleBlank() {
+    blank = !blank;
+    localStorage.setItem('blank', blank);
+
+    if (blank) {
+        button.innerHTML = "true";
+    } else {
+        button.innerHTML = "off";
+    }
+}
+
+function loadBlank() {
+    var isBlank = localStorage.getItem('blank') === 'true';
+
+    if (isBlank) {
+        var win = window.open();
+        var iframe = win.document.createElement('iframe');
+        iframe.style.position = "fixed";
+        iframe.style.top = "0";
+        iframe.style.left = "0";
+        iframe.style.bottom = "0";
+        iframe.style.right = "0";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.border = "none";
+        iframe.style.margin = "0";
+        iframe.style.padding = "0";
+        iframe.style.overflow = "hidden";
+        iframe.style.zIndex = "999999";
+        iframe.src = window.location.href;
+        win.document.body.appendChild(iframe);
+    }
+}
