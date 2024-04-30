a437c6ff13e23d3d16d69a5bcd092ffb716ad75f
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-04-29 21:58:34 -0600

    may not work

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,28 +1,5 @@
 let main = 'https://bare.benrogo.net';
 
-async function isURLReachable(url) {
-    try {
-        const response = await fetch(url);
-        return response.ok;
-    } catch (error) {
-        return false;
-    }
-}
-
-isURLReachable('https://bare.benrogo.net')
-    .then(online => {
-        if (online) {
-            console.log('Primary URL is reachable, using it.');
-        } else {
-            console.log('Primary URL is not reachable, using backup URL.');
-            main = 'https://history.about.dansted.org/o/';
-        }
-    })
-    .catch(error => {
-        console.error('Error occurred while checking URL:', error);
-        console.log('Using backup URL.');
-    });
-
 self.__uv$config = {
     prefix: '/static/tiw/',
     bare: main,
