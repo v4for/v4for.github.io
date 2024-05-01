500435c9c5b4f19e00192194e0907aaea5bba78a
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-05-01 13:50:44 -0600

    Update tabs.js

--- gxmes/tabs.js
+++ gxmes/tabs.js
@@ -25,8 +25,33 @@ function togglePopupButton() {
 
 function toggleButton() {
     useIframeButton.classList.toggle('active');
-    toggleLinkBehavior();
+    var links = document.querySelectorAll('a');
+
+    links.forEach(function(link) {
+        link.onclick = function(event) {
+            event.preventDefault(); 
+            const html = '<html><title>IT work</title><iframe src="' + link.href + '" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></html>';
+            const blob = new Blob([html], {type: 'text/html'});
+            var win = window.open();
+            var iframe = win.document.createElement('iframe');
+            iframe.style.position = "fixed";
+            iframe.style.top = "0";
+            iframe.style.left = "0";
+            iframe.style.bottom = "0";
+            iframe.style.right = "0";
+            iframe.style.width = "100%";
+            iframe.style.height = "100%";
+            iframe.style.border = "none";
+            iframe.style.margin = "0";
+            iframe.style.padding = "0";
+            iframe.style.overflow = "hidden";
+            iframe.style.zIndex = "999999";
+            iframe.src = window.URL.createObjectURL(blob);
+            win.document.body.appendChild(iframe);
+        };
+    });
 }
+
 
 function toggle2() {
     noButton.classList.toggle('active');
