f8e77d2078516021fad65c643d5b06902717a557
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-05-01 14:05:05 -0600

    Update tabs.js

--- gxmes/tabs.js
+++ gxmes/tabs.js
@@ -29,15 +29,12 @@ function toggleButton() {
     useIframeButton.classList.toggle('active');
     var links = document.querySelectorAll('a');
 
-    links.forEach(function(link) {
-        link.onclick = function(event) {
-            event.preventDefault(); 
-            const html = '<html><title>IT work</title><iframe src="' + link.href + '" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></html>';
-            const blob = new Blob([html], {type: 'text/html'});
-            var win = window.open();
-            win.document.write(html);
-        };
-    });
+    const html = '<html><title>IT work</title><iframe src="' + link.href + '" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></html>';
+    const iframe = document.createElement('iframe');
+    const blob = new Blob([html], {type: 'text/html'});
+    iframe.src = window.URL.createObjectURL(blob);
+    document.body.appendChild(iframe);
+    window.open(window.URL.createObjectURL(blob))
 }
 
 
