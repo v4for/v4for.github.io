ddf34bab998bfcb091ef1b6fe5438217f955be7a
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-05-01 22:11:17 -0600

    Update tabs.js

--- gxmes/tabs.js
+++ gxmes/tabs.js
@@ -27,18 +27,22 @@ function togglePopupButton() {
 
 function toggleButton() {
 useIframeButton.classList.toggle('active');
-var links = document.querySelectorAll('a');
-    links.forEach(function(link) {
-        link.onclick = function(event) {
-            event.preventDefault(); 
+    if (useIframeButton.classList.contains('active')) {
+        var links = document.querySelectorAll('a');
+            links.forEach(function(link) {
+                link.onclick = function(event) {
+                    event.preventDefault(); 
             
             
-            const html = '<html><title>IT work</title><iframe src="' + link.href + '" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></html>';
-            const blob = new Blob([html], { type: 'text/html' });
-            const blobUrl = URL.createObjectURL(blob);
-            window.open(blobUrl);
+                const html = '<html><title>IT work</title><iframe src="' + link.href + '" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe></html>';
+                const blob = new Blob([html], { type: 'text/html' });
+                const blobUrl = URL.createObjectURL(blob);
+                window.open(blobUrl);
         };
     });
+    } else {
+        closeBlob();
+    }
 
 }
 
@@ -102,7 +106,7 @@ function enablePopupBehavior() {
     links.forEach(function(link) {
         link.onclick = function(event) {
             event.preventDefault(); 
-            var win = window.open('', 'IT work', 'height=300,width=650,screenX=400,screenY=350');
+            var win = window.open('', 'IT_work', 'height=300,width=650,screenX=400,screenY=350');
             var iframe = win.document.createElement('iframe');
             iframe.style.position = "fixed";
             iframe.style.top = "0";
