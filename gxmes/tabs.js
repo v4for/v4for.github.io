1fc509a81702557d58dc83a536ea79e3f27f4efc
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-05-01 22:02:29 -0600

    Update tabs.js

--- gxmes/tabs.js
+++ gxmes/tabs.js
@@ -97,6 +97,29 @@ function closeBlob() {
     });
 }
 
+function enablePopupBehavior() {
+        var links = document.querySelectorAll('a');
+    links.forEach(function(link) {
+        link.onclick = function(event) {
+            event.preventDefault(); 
+            var win = window.open('', 'IT work', 'height=300,width=650,screenX=400,screenY=350');
+            var iframe = win.document.createElement('iframe');
+            iframe.style.position = "fixed";
+            iframe.style.top = "0";
+            iframe.style.left = "0";
+            iframe.style.bottom = "0";
+            iframe.style.right = "0";
+            iframe.style.width = "100%";
+            iframe.style.height = "100%";
+            iframe.style.border = "none";
+            iframe.style.margin = "0";
+            iframe.style.padding = "0";
+            iframe.style.overflow = "hidden";
+            iframe.style.zIndex = "999999";
+            iframe.src = link.href;
+            win.document.body.appendChild(iframe);
+        };
+    });}
 
 
 function disablePopupBehavior() {
