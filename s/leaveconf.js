111c65509e98ee48c94978617c0ada3dc4c271e1
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-05-02 08:39:12 -0600

    small fix

--- s/leaveconf.js
+++ s/leaveconf.js
@@ -1,12 +1,6 @@
-window.addEventListener('beforeunload', function(e) {
-
-    
-    if (!localStorage.getItem('leaveConf') || localStorage.getItem('leaveConf') !== 'true') {
-        if (e.target.tagName.toLowerCase() === 'a') {
-            return;
-        } else {
-            e.preventDefault();
-            e.returnValue = 'Are you leaving? Or is your extention making you?';
-        }
-    }
-});
+if (!localStorage.getItem('leaveConf') || localStorage.getItem('leaveConf') !== 'true') {
+    window.addEventListener('beforeunload', function(e) {
+        e.preventDefault();
+        e.returnValue = ''; 
+    });
+}
