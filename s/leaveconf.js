3c41db1f02db660caeeda42f8a85ca1e3a464ae3
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-05-02 08:26:56 -0600

    why didnt it commt the first time

--- s/leaveconf.js
+++ s/leaveconf.js
@@ -1,6 +1,10 @@
 window.addEventListener('beforeunload', function(e) {
+    if (e.target.tagName.toLowerCase() === 'a') {
+        return;
+    }
+    
     if (!localStorage.getItem('leaveConf') || localStorage.getItem('leaveConf') !== 'true') {
         e.preventDefault();
-        e.returnValue = '';
+        e.returnValue = 'Are you leaving? Or is your extention making you?';
     }
 });
