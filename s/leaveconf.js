3ce7fb277e3d4e189962337194b4fcf85e58f89c
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-05-02 08:52:31 -0600

    console.log stugg

--- s/leaveconf.js
+++ s/leaveconf.js
@@ -1,3 +1,5 @@
+console.log(localStorage.getItem('leaveConf'));
+
 if (localStorage.getItem('leaveConf') == 'true') {
     window.addEventListener('beforeunload', function(e) {
         e.preventDefault();
--- s/settings.js
+++ s/settings.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     toggleButton.addEventListener('click', function() {
         toggleBlank();
+        console.log(localStorage.getItem('leaveConf'));
     });
 
     function toggleBlank() {
