89154cc2097cf7dddae2bedaf2e93b7a402ef523
Vafor <68476283+maxwell-com@users.noreply.github.com>
2024-05-02 08:34:18 -0600

    some small fized

--- s/leaveconf.js
+++ s/leaveconf.js
@@ -1,10 +1,12 @@
 window.addEventListener('beforeunload', function(e) {
-    if (e.target.tagName.toLowerCase() === 'a') {
-        return;
-    }
+
     
     if (!localStorage.getItem('leaveConf') || localStorage.getItem('leaveConf') !== 'true') {
-        e.preventDefault();
-        e.returnValue = 'Are you leaving? Or is your extention making you?';
+        if (e.target.tagName.toLowerCase() === 'a') {
+            return;
+        } else {
+            e.preventDefault();
+            e.returnValue = 'Are you leaving? Or is your extention making you?';
+        }
     }
 });
