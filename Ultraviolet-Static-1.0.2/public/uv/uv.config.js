c090c69dba9133eaea4d719877671979d47abb91
Vafor <68476283+v4for@users.noreply.github.com>
2024-05-13 10:03:38 -0600

    standalone

--- Ultraviolet-Static-1.0.2/public/uv/uv.config.js
+++ Ultraviolet-Static-1.0.2/public/uv/uv.config.js
@@ -1,13 +1,11 @@
-// This file overwrites the stock UV config.js
-
+/*global Ultraviolet*/
 self.__uv$config = {
-  prefix: "/uv/service/",
-  bare: "/bare/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+    prefix: '/service/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv.handler.js',
+    client: '/uv.client.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
 };
