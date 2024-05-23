0445c31e2e8744ef52c4b83cb2148d6037540442
Vafor <68476283+v4for@users.noreply.github.com>
2024-05-23 17:00:13 -0600

    skish

--- Vafor_IT-main/10-minutes-till-dawn/js/space.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    if (event.code === 'Space') {
-        event.preventDefault(); // Prevent default spacebar behavior (scrolling)
-        const gameContainer = document.getElementById('gameContainer');
-        if (gameContainer) {
-            gameContainer.click();
-        }
-    }
-});
