59557c715c9f9c6ec65fa130236fe51945c00580
Vafor <68476283+v4for@users.noreply.github.com>
2024-05-23 22:08:17 -0600

    wowowo this is gonna render lots of games usles

--- gxmes/vercel.js
+++ gxmes/vercel.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const keywords = ['ovo', 'basketball', 'vex 6'];
-    
+    const keywords = ['ovo', 'basketball', 'vex 6' , 'slope'];
+
     if (window.location.href.includes('vercel.app')) {
         const elements = document.body.getElementsByTagName('*');
-        
+
         for (let element of elements) {
             for (let keyword of keywords) {
                 if (element.textContent.includes(keyword)) {
-                    let parentDiv = element.closest('div');
-                    if (parentDiv) {
-                        parentDiv.style.display = 'none';
+                    let targetDiv = element.closest('div[style*="margin: 10px;"]');
+                    if (targetDiv) {
+                        targetDiv.style.display = 'none';
                     }
                 }
             }
