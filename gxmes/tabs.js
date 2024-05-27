fc8acac765567c139f6080e902738ae7d3e9e252
Vafor <68476283+v4for@users.noreply.github.com>
2024-05-27 16:02:12 -0600

    more things

--- gxmes/tabs.js
+++ gxmes/tabs.js
@@ -88,3 +88,23 @@ function openPopup() {
         }); 
     }, 50);
 } 
+
+//search
+function search() {
+    var keyword = prompt("What is blud searching for?");
+
+    const elements = document.body.getElementsByTagName('*');
+
+    for (let element of elements) {
+        if (element.textContent.includes(keyword)) {
+            let targetDiv = element.closest('div[style*="margin: 10px;"]');
+            if (targetDiv) {
+                for (let el of elements) {
+                    if (el !== targetDiv && !el.contains(targetDiv)) {
+                        el.style.display = 'none';
+                    }
+                }
+            }
+        }
+    }
+}
