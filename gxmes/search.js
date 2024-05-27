88785f5a3992c8ef7b35ff6111da21e8491f3955
Vafor <68476283+v4for@users.noreply.github.com>
2024-05-27 15:21:46 -0600

    Update search.js

--- gxmes/search.js
+++ gxmes/search.js
@@ -4,11 +4,16 @@ function search() {
     const elements = document.body.getElementsByTagName('*');
 
     for (let element of elements) {
-            if (element.textContent.includes(keyword)) {
-                let targetDiv = element.closest('div[style*="margin: 10px;"]');
-                if (targetDiv) {
-                    targetDiv.style.display = 'none';
+        if (element.textContent.includes(keyword)) {
+            let targetDiv = element.closest('div[style*="margin: 10px;"]');
+            if (targetDiv) {
+                for (let el of elements) {
+                    if (el !== targetDiv && !el.contains(targetDiv)) {
+                        el.style.display = 'none';
+                    }
                 }
             }
+        }
     }
 }
+
