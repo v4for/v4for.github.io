155fb61dc06e2df9431b8391413b76b3f7e83715
Vafor <68476283+v4for@users.noreply.github.com>
2024-05-27 18:48:00 -0600

    i got semi destracted

--- gxmes/search.js
+++ gxmes/search.js
@@ -5,17 +5,11 @@ function searchmedaddy() {
     const elements = document.body.getElementsByTagName('*');
 
     for (let element of elements) {
-        let keywordFound = false;
         for (let keyword of keywordsArray) { 
             if (element.textContent.includes(keyword)) {
-                keywordFound = true;
-                break;
-            }
-        }
-        if (!keywordFound) {
-            let targetDiv = element.closest('div[style*="margin: 10px;"]');
-            if (targetDiv) {
-                targetDiv.style.display = 'none';
+                //nothing
+            } else {
+                element.closest('div[style*="margin: 10px;"]').style.display = 'none';
             }
         }
     }
