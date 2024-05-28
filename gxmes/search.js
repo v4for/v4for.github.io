ab723784066119dc1131b3797c16a815d8c79f1c
Vafor <68476283+v4for@users.noreply.github.com>
2024-05-27 19:06:26 -0600

    Message (commit message)

--- gxmes/search.js
+++ gxmes/search.js
@@ -5,11 +5,17 @@ function searchmedaddy() {
     const elements = document.body.getElementsByTagName('*');
 
     for (let element of elements) {
+        let hideElement = true; // Assume we'll hide the element by default
         for (let keyword of keywordsArray) { 
             if (element.textContent.includes(keyword)) {
-                //nothing
-            } else {
-                element.closest('div[style*="margin: 10px;"]').style.display = 'none';
+                hideElement = false; // Don't hide if any keyword is found
+                break; // No need to check further if keyword found
+            }
+        }
+        if (hideElement) {
+            const closestDiv = element.closest('div[style*="margin: 10px;"]');
+            if (closestDiv !== null) {
+                closestDiv.style.display = 'none';
             }
         }
     }
