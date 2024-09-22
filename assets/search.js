e35513a947ced92fd90c834ae7da9524fcbbfd1b
Vafor <68476283+v4for@users.noreply.github.com>
2024-09-22 17:08:23 -0600

    yes

--- assets/search.js
+++ assets/search.js
@@ -11,6 +11,19 @@ function displayResults() {
   if (inputField.value === '') { 
     alert("Say something!");
   } else {
+    //also need modal code
     document.getElementById("whatisearched").innerHTML = inputField.value;
+
+      const modal = document.getElementById('modal');
+      modal.style.display = 'block';
+      const modalContent = modal.querySelector('.modal-content');
+      modalContent.style.animation = 'none';
+      modalContent.offsetHeight; 
+      modalContent.style.animation = '';
   }
+}
+
+function closeModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
 }
