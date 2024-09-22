54335419ffde5d7bad8339101bbc31f18b82a9a5
Vafor <68476283+v4for@users.noreply.github.com>
2024-09-22 16:49:51 -0600

    something

--- assets/search.js
+++ assets/search.js
@@ -1,34 +1,16 @@
 document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13) {
-      
-      search = document.getElementsByClassName('userInput').value;
-  
-      const commonOutput = `<p> guest@terminal.maxwell.com$~ ${command}</p>`;
-      
-      if (command.startsWith('echo ')) {
-        const echoedText = command.substring(5); // Extract text after 'echo '
-        outputArea.innerHTML += `<p> guest@terminal.maxwell.com$~ ${command}</p> ` + `<p>${echoedText}</p>`;
-      } else if (command === 'date') {
-        const d = new Date();
-        outputArea.innerHTML += `${commonOutput} 
-          <p>${d}</p>`;
-      } else if (command === 'help') {
-        outputArea.innerHTML += `<p> guest@terminal.maxwell.com$~ ${command}</p>` + help;
-      } else if (command === 'clear') {
-        outputArea.innerHTML = ''; 
-        var commandLine = document.getElementById("Command_Line");
-        commandLine.insertAdjacentElement('beforebegin', userInputContainer);
-      } else if (command === 'echo') {
-        outputArea.innerHTML += `<p> guest@terminal.maxwell.com$~ ${command}</p>` + echo;
-      } else if (command === 'whoami') {
-        outputArea.innerHTML += `<p> guest@terminal.maxwell.com$~ ${command}</p> <br>` + whoami1;
+  if (event.keyCode === 13) {
+      let search = document.getElementsByClassName('searchTerm')[0].value;
+
+      if (search === '') {
+          alert('Please search for something');
       } else {
-        outputArea.innerHTML += `<p> guest@terminal.maxwell.com$~ ${command}</p> ` + `<p>Command ${command} is not recognized... for now.</p>`;
-      }    
-      
-      outputArea.innerHTML += '<hr class="invisible-hr">';
-      document.getElementById('userInput').value = '';
-      
-     
-    }
-  });
+          displayResults(search); 
+      }
+  }
+});
+
+
+function displayResults(search) {
+  document.getElementById('whatisearched').innerText = search; 
+}
