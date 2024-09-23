b6608befff39d647144755dd20e804218138a6d2
Vafor <68476283+v4for@users.noreply.github.com>
2024-09-22 20:58:28 -0600

    silly silly

--- assets/search.js
+++ assets/search.js
@@ -1,25 +1,25 @@
-document.getElementById('inputField').addEventListener('keydown', function(event) {
+document.getElementById('searchTerm').addEventListener('keydown', function(event) {
   if (event.key === 'Enter') {
       displayResults();
   }
 });
 
 function displayResults() {
-  const inputField = document.getElementsByClassName('inputField'); 
-  console.log('Entered text:', inputField.value);
+  const inputField = document.getElementsByClassName('searchTerm'); 
 
-  if (inputField.value === '') { 
+  if (!inputField.value) { 
     alert("Say something!");
   } else {
-    //also need modal code
-    document.getElementById("whatisearched").innerHTML = inputField.value;
-
+    console.log(inputField.value)
+    //document.getElementById("whatisearched").innerHTML = inputField.value;
+      /*
       const modal = document.getElementById('modal');
       modal.style.display = 'block';
       const modalContent = modal.querySelector('.modal-content');
       modalContent.style.animation = 'none';
       modalContent.offsetHeight; 
       modalContent.style.animation = '';
+      */
   }
 }
 
