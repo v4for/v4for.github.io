7caf9a715a5f65a0f1bdcd7111490db8078522c7
Vafor <68476283+v4for@users.noreply.github.com>
2024-09-23 16:23:28 -0600

    s

--- assets/search.js
+++ assets/search.js
@@ -6,22 +6,6 @@ function displayResults() {
   } else {
     console.log(inputField.value)
       document.getElementById("whatisearchedfor").innerHTML = inputField.value;
-
-      const filter = this.value.toLowerCase();
-      const collection = document.getElementById('collection');
-      const divs = collection.getElementsByTagName('div');
-
-      for (let i = 0; i < divs.length; i++) {
-          const description = divs[i].getElementsByClassName('description')[0];
-          if (description) {
-              const textValue = description.textContent || description.innerText;
-              if (textValue.toLowerCase().includes(filter)) {
-                  divs[i].style.display = 'block'; // Show the div
-              } else {
-                  divs[i].style.display = 'none'; // Hide the div
-              }
-          }
-      }
       
       const modal = document.getElementById('modal');
       modal.style.display = 'block';
