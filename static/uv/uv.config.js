6566c3277922058d6c3b8d8ca740780e9c192663
Vafor <68476283+v4for@users.noreply.github.com>
2024-09-23 15:49:32 -0600

    error

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://decimals.mountzion.nsw.edu.au/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
