1415bbd6cbb4473a1b46bc8501bb5a5db2138eb6
Vafor <68476283+v4for@users.noreply.github.com>
2024-10-02 22:04:08 -0600

    this shoudl work

--- gxmes/sighn.js
+++ gxmes/sighn.js
@@ -1,2 +1,14 @@
 const searched = new URL(window.location).searchParams.get('searched');
 console.log(searched); // search wohoo
+
+function hidesomediv(text) {
+    const descriptions = document.querySelectorAll('.description');
+    descriptions.forEach(description => {
+      if (!description.textContent.includes(text)) {
+        description.parentElement.style.display = 'none';
+      }
+    });
+  }
+
+ 
+  hidesomediv(searched);
