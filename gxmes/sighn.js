da138747e9a1df1a2e18614aa938d529f167b784
Vafor <68476283+v4for@users.noreply.github.com>
2024-10-02 22:11:42 -0600

    has to wait for dom to load what a loser

--- gxmes/sighn.js
+++ gxmes/sighn.js
@@ -1,14 +1,15 @@
-const searched = new URL(window.location).searchParams.get('searched');
-console.log(searched); // search wohoo
+document.addEventListener('DOMContentLoaded', () => {
+    const searched = new URL(window.location).searchParams.get('searched');
+    console.log(searched); // search wohoo
 
-function hidesomediv(text) {
-    const descriptions = document.querySelectorAll('.description');
-    descriptions.forEach(description => {
-      if (!description.textContent.includes(text)) {
-        description.parentElement.style.display = 'none';
-      }
-    });
-  }
+    function hidesomediv(text) {
+        const descriptions = document.querySelectorAll('.description');
+        descriptions.forEach(description => {
+            if (!description.textContent.includes(text)) {
+                description.parentElement.style.display = 'none';
+            }
+        });
+    }
 
- 
-  hidesomediv(searched);
+    hidesomediv(searched);
+});
