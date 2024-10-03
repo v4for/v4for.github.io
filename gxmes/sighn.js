728bcc697d279913fcfc037a7d779109c3a0c3ff
Vafor <68476283+v4for@users.noreply.github.com>
2024-10-03 08:39:42 -0600

    mmg

--- gxmes/sighn.js
+++ gxmes/sighn.js
@@ -1,3 +1,5 @@
+let matchFound = false;
+
 document.addEventListener('DOMContentLoaded', () => {
     const searched = new URL(window.location).searchParams.get('searched');
     console.log('Searched term:', searched); //search wohoo
@@ -6,16 +8,26 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!text) {
             return; 
         }
+
+
         document.getElementById("hidethis").style.display = "none";
         document.getElementById("searchtitle").innerHTML = "Results for: " + searched;
 
         const descriptions = document.querySelectorAll('.description');
+
         descriptions.forEach(description => {
-            if (!description.textContent.toLowerCase().includes(text.toLowerCase())) {
+            if (description.textContent.toLowerCase().includes(text.toLowerCase())) {
+                matchFound = true;
+            } else {
                 description.parentElement.style.display = 'none';
             }
         });
     }
 
     hidesomediv(searched);
+
+    if (!matchFound) {
+        alert("No matches found for " + searched + ". redircted you away")
+        window.location = "/gxmes"
+    }
 });
