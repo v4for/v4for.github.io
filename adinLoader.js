629cea0a2fdf08ea24dadc9d23a69bac26c9dde0
Vafor <68476283+v4for@users.noreply.github.com>
2024-10-04 09:32:45 -0600

    asd

--- adinLoader.js
+++ adinLoader.js
@@ -1,7 +1,7 @@
 let currentUrl = window.location.href;
 
-let stringToRemove = "/adinGameLoader.js";
+let stringToRemove = "vafor_main-it/adinGameLoader.js";
 
 let newUrl = currentUrl.replace(stringToRemove, '');
 
-const getAdinDomain = newUrl+ 'a3f5d9e7c2b8a4e1f3d5c7e9b0a4f6c8/preroll/adinGameLoader.js';
+const getAdinDomain = newUrl+ '/preroll/adinGameLoader.js';
