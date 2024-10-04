b0ad587ec20ee16221014284cd6379d1a7a8aace
Vafor <68476283+v4for@users.noreply.github.com>
2024-10-04 09:26:34 -0600

    fix

--- adinLoader.js
+++ adinLoader.js
@@ -4,4 +4,4 @@ let stringToRemove = "/adinGameLoader.js";
 
 let newUrl = currentUrl.replace(stringToRemove, '');
 
-const getAdinDomain = newUrl+ 'preroll/adinGameLoader.js';
+const getAdinDomain = newUrl+ 'a3f5d9e7c2b8a4e1f3d5c7e9b0a4f6c8/preroll/adinGameLoader.js';
