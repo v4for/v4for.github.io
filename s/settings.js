97760ac5ff52492eeb5973961d29de3f4d9d6018
Vafor <68476283+v4for@users.noreply.github.com>
2024-10-10 16:10:08 -0600

    Update settings.js

--- s/settings.js
+++ s/settings.js
@@ -1,33 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.getElementById('toggleButton');
-
-    var load = localStorage.getItem('leaveConf');
-    if (load === null) {
-        load = false;
-        localStorage.setItem('leaveConf', load);
-    } else {
-        load = load === 'true';
-    }
-    
-    toggleButton.innerHTML = load.toString(); 
-
-    toggleButton.addEventListener('click', function() {
-        toggleBlank();
-        console.log(localStorage.getItem('leaveConf'));
-    });
-
-    function toggleBlank() {
-        if (load) {
-            load = false;
-            toggleButton.innerHTML = 'false';
-        } else {
-            load = true;
-            toggleButton.innerHTML = 'true';
-        }
-        localStorage.setItem('leaveConf', load); 
-    }
-});
-
 function blank2true() {
     if (localStorage.getItem('blank2') === null) {
         localStorage.setItem('blank2', 'true');
