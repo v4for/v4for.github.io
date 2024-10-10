e8dd3bad2630cd30b5d2a29b0f8f20cde65d10f0
Vafor <68476283+v4for@users.noreply.github.com>
2024-10-10 16:11:14 -0600

    Delete s/leaveconf.js

--- s/leaveconf.js
+++
@@ -1,9 +0,0 @@
-console.log(localStorage.getItem('leaveConf'));
-
-if (localStorage.getItem('leaveConf') == 'true') {
-    window.addEventListener('beforeunload', function(e) {
-        e.preventDefault();
-        e.returnValue = ''; 
-    });
-}
-
