c7cbe4625f2fa142fc8131117d2718601bd106a9
Vafor <68476283+v4for@users.noreply.github.com>
2024-10-23 10:41:59 -0600

    xdgsdfjkghkjsdffgkjsdfg

---
+++ Vafor_IT-main/idle-breakout-main/idle-breakout-main/html5game/vph_sitelock.js
@@ -0,0 +1,42 @@
+/* version 0.1
+
+	made by Kodiqi
+	
+*/
+
+function sl_get_info() {
+        console.log("Checking...");
+        console.log(location.ancestorOrigins[0]);
+        console.log("Checking again...");
+        console.log(document.referrer);
+		gml_Script_gmcallback_site_details(-1, -1, location.ancestorOrigins[0], document.referrer);
+    }
+
+
+function unblockedGetParentUrl()
+{
+	var isInIframe = (parent!==window),parentUrl=null;if(isInIframe){parentUrl=document.referrer}
+			
+	if (parentUrl != null && !parentUrl.includes("http"))
+	{
+		return null;
+	}
+			
+	return parentUrl;
+}
+
+function sl_fn() {
+
+	const xr = window.location;
+	const xr2 = "window.location";
+			
+	const currentIframeHref = unblockedGetParentUrl();
+	if (currentIframeHref != null)
+	{
+		var referrer = new URL(currentIframeHref).hostname;
+		if (referrer !== xr && referrer !== xr2)
+		{
+			window.location.href = 'https://www.maxwellstevenson.com';
+		}
+	}
+}
