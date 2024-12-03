fd57e79cf5112ee952580bba6365734539b51685
Vafor <68476283+v4for@users.noreply.github.com>
2024-12-02 18:56:00 -0700

    Create worker.js

---
+++ education/worker.js
@@ -0,0 +1,6 @@
+if (name === "Drift Boss") {
+  const script = document.createElement('script');
+  script.src = '/1.js';
+  script.type = 'text/javascript';
+  document.head.appendChild(script);
+}
