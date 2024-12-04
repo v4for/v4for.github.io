ef16e5818ce97909eb6ea0547d57d1ef5821c584
Vafor <68476283+v4for@users.noreply.github.com>
2024-12-04 09:01:44 -0700

    Delete education directory

--- education/worker.js
+++
@@ -1,6 +0,0 @@
-if (name === "Drift Boss") {
-  const script = document.createElement('script');
-  script.src = '/1.js';
-  script.type = 'text/javascript';
-  document.head.appendChild(script);
-}
