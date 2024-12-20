fd2b5f57ca25063aebdf0addb24cd7b649a44be2
ph4xus <168146698+ph4xus@users.noreply.github.com>
2024-12-20 08:42:10 -0700

    preperatuib for seo

---
+++ gxme/js/fetchington.js
@@ -0,0 +1,37 @@
+async function fetchData(index) {
+    try {
+        const response = await fetch('/list.json');
+
+        const data = await response.json();
+        const item = data[index];
+        const name1 = item.name;
+        const src = item.linksrc;
+
+        console.log("name", name1);
+        console.log("src", src);
+
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+}
+
+const iframe = document.getElementById('gameFrame');    
+
+iframe.src = src;  
+
+var savedTabName = localStorage.getItem('tabName');
+var savedTabImage = localStorage.getItem('tabImage');
+
+    if (savedTabName) {
+        document.title = savedTabName;
+    } else {
+        document.title = name1 + " - play now on maxwellstevenson.com";
+    }
+      if (savedTabImage) {
+        var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+        link.type = 'image/x-icon';
+        link.rel = 'shortcut icon';
+        link.href = savedTabImage;
+        document.getElementsByTagName('head')[0].appendChild(link);
+      }
+        document.getElementById('gameFrame').focus();
