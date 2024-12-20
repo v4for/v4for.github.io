06f106f94da81bd1154a77a7fb8a12c01ac24eb7
ph4xus <168146698+ph4xus@users.noreply.github.com>
2024-12-20 09:54:08 -0700

    dd

--- gxme/js/fetchington.js
+++ gxme/js/fetchington.js
@@ -1,7 +1,6 @@
 async function fetchData(index) {
     try {
         const response = await fetch('/list.json');
-
         const data = await response.json();
         const item = data[index];
         const name1 = item.name;
@@ -10,32 +9,38 @@ async function fetchData(index) {
         console.log("name", name1);
         console.log("src", src);
 
+        // Set iframe src
+        const iframe = document.getElementById('gameFrame');    
+        iframe.src = src;
+
+        // Set tab name and image
+        var savedTabName = localStorage.getItem('tabName');
+        var savedTabImage = localStorage.getItem('tabImage');
+
+        if (savedTabName) {
+            document.title = savedTabName;
+        } else {
+            document.title = name1 + " - play now on maxwellstevenson.com";
+        }
+
+        if (savedTabImage) {
+            var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+            link.type = 'image/x-icon';
+            link.rel = 'shortcut icon';
+            link.href = savedTabImage;
+            document.getElementsByTagName('head')[0].appendChild(link);
+        }
+
+        document.getElementById('gameFrame').focus();
+
+        // Add keywords
+        const keywords = 'gxme, gxmes, ' + name1 + ' unblxcked, ' + name1 + ' maxwellstevenson.com, Vafor, Vafor IT, Vafor IT Work, EZ 66, ' + name1;
+        addKeywords(keywords);
+
     } catch (error) {
         console.error('Fetch error:', error);
     }
 }
-
-const iframe = document.getElementById('gameFrame');    
-
-iframe.src = item.linksrc;
-
-var savedTabName = localStorage.getItem('tabName');
-var savedTabImage = localStorage.getItem('tabImage');
-
-    if (savedTabName) {
-        document.title = savedTabName;
-    } else {
-        document.title = item.name; + " - play now on maxwellstevenson.com";
-    }
-      if (savedTabImage) {
-        var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-        link.type = 'image/x-icon';
-        link.rel = 'shortcut icon';
-        link.href = savedTabImage;
-        document.getElementsByTagName('head')[0].appendChild(link);
-      }
-        document.getElementById('gameFrame').focus();
-
 
 function addKeywords(keywords) {
     const meta = document.createElement('meta');
@@ -43,6 +48,3 @@ function addKeywords(keywords) {
     meta.content = keywords;
     document.head.appendChild(meta);
 }
-
-const keywords = 'gxme, gxmes, ' + name1 + ' unblxcked, ' + name1 + ' maxwellstevenson.com, Vafor, Vafor IT, Vafor IT Work, EZ 66, ' + name1;
-addKeywords(keywords);
