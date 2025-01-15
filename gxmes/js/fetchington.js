10a15e52dfa2a9417c794922d2199742aa3c637e
ph4xus <168146698+ph4xus@users.noreply.github.com>
2025-01-15 13:21:32 -0700

    comment

--- gxmes/js/fetchington.js
+++ gxmes/js/fetchington.js
@@ -102,3 +102,24 @@ function toggleFullscreen() {
                 iframe.msRequestFullscreen();
             }
         }
+
+
+/*
+ var savedTabName = localStorage.getItem('tabName');
+        var savedTabImage = localStorage.getItem('tabImage');
+
+        if (savedTabName) {
+            document.title = savedTabName;
+        } else {
+            document.title = name1 + " - play now on maxwellstevenson.com";
+        }
+
+        if (savedTabImage) {
+            var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+            link.type = 'image/x-icon';
+            link.rel = 'shortcut icon';
+            link.href = savedTabImage;
+            document.getElementsByTagName('head')[0].appendChild(link);
+        }
+
+*/
